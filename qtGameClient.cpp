7244683b19691634d84e2ea01b21c53ff464ece0
Nadnerb <brendandorr@gmail.com>
2010-04-01 16:54:34 +0000

    Set up LoadClone construction/sending, not finished - need a way to convert plUoids

--- qtGameClient.cpp
+++ qtGameClient.cpp
@@ -2,6 +2,7 @@
 #include "qtGameClient.h"
 #include <core/Stream/hsRAMStream.h>
 #include <core/PRP/NetMessage/plNetMsgLoadClone.h>
+#include <core/PRP/Message/plLoadAvatarMsg.h>
 
 Q_DECLARE_METATYPE(plUuid);
 
@@ -39,8 +40,54 @@ void qtGameClient::joinAge(hsUint32 serverAddr, hsUint32 playerId, hsUint32 mcpI
 void qtGameClient::onJoinAgeReply(hsUint32 transId, ENetError result) {
     if(result == kNetSuccess) {
         qWarning("Joined Age");
-        //plNetMsgLoadClone loadClone;
-        //propagateMessage(&loadClone);
+        plUoid clientMgr;
+        clientMgr.setName("kNetClientMgr_KEY");
+        clientMgr.setID(0);
+        clientMgr.setType(0x0052); //plNetClientMger
+        plLocation clientMgrLoc(pvLive);
+        clientMgrLoc.setVirtual();
+        clientMgrLoc.setFlags(0x0000);
+        clientMgr.setLocation(clientMgrLoc);
+        plUoid playerKey;
+        if(true) {
+            playerKey.setName("Male");
+            playerKey.setID(78);
+            plLocation playerKeyLoc(pvLive);
+            playerKeyLoc.setPageNum(1);
+            playerKeyLoc.setSeqPrefix(-6);
+            playerKeyLoc.setFlags(0x0004);
+            //playerKey.setLocation(playerKey);
+        }
+        playerKey.setType(0x0001); //plSceneObject
+        playerKey.setCloneIDs(2, fPlayerId); //not sure what the 2 signifies
+        plUoid avMgr;
+        avMgr.setName("kAvatarMgr_KEY");
+        plLocation avMgrLoc(pvLive);
+        avMgrLoc.setVirtual();
+        avMgrLoc.setFlags(0x0000);
+        avMgr.setLocation(avMgrLoc);
+        avMgr.setType(0x00F4); //plAvatarMgr
+        avMgr.setID(0);
+        plLoadAvatarMsg loadAvMsg;
+        //loadAvMsg.addReceiver(clientMgr);
+        loadAvMsg.setBCastFlags(0x00000840);
+        //loadAvMsg.setCloneKey(playerKey);
+        //loadAvMsg.setRequestorKey(avMgr);
+        loadAvMsg.setOriginatingPlayerID(fPlayerId);
+        loadAvMsg.setUserData(0);
+        loadAvMsg.setValidMsg(1);
+        loadAvMsg.setIsLoading(1);
+        loadAvMsg.setIsPlayer(1);
+        plNetMsgLoadClone loadClone;
+        loadClone.setPlayerID(fPlayerId);
+        loadClone.setMessage(&loadAvMsg);
+        loadClone.setIsPlayer(1);
+        loadClone.setIsLoading(1);
+        loadClone.setIsInitialState(0);
+        plNetMsgObjectHelper helper;
+        helper.setUoid(playerKey);
+        loadClone.setObject(helper);
+        propagateMessage(&loadClone);
     }else{
         qWarning("Join Age Failed (%s)", GetNetErrorString(result));
     }
