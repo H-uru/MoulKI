533ea1aca31b80173e1075b56ba9a2027d6e9aff
Nadnerb <brendandorr@gmail.com>
2010-05-07 21:24:17 -0400

    Updated for build 893

--- MoulKI.h
+++ MoulKI.h
@@ -6,7 +6,7 @@
 #include "qtGameClient.h"
 #include "qtVault.h"
 
-#define BUILD_NUMBER 890
+#define BUILD_NUMBER 893
 #define MOUL_HOST "184.73.198.22"
 #include "keys.cpp"
 static const plUuid s_moulUuid("ea489821-6c35-4bd0-9dae-bb17c585e680");
