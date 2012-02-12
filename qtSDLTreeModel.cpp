f88174dd2accd81ec6b37f33c512a48b15cb6fcb
NadnerbD <brendandorr@gmail.com>
2012-02-12 05:47:45 -0500

    First attempt at a viable abstraction The limitations of QModelIndex are rather annoying. I'd like to have more than just an int OR a pointer

--- qtSDLTreeModel.cpp
+++ qtSDLTreeModel.cpp
@@ -1,27 +1,54 @@
 #include "qtSDLTreeModel.h"
+#include "SDL/plStateDataRecord.h"
+#include "SDL/plStateVariable.h"
 
 qtSDLTreeModel::qtSDLTreeModel(plStateDataRecord* sdl) :
-    QAbstractItemModel(NULL), sdl(sdl)
+    QAbstractItemModel(0), sdl(sdl)
 {
+}
 
+QModelIndex qtSDLTreeModel::ICreateIndex(int row, int column, QModelIndex parent, void *ptr, ItemType type) {
+    SDLModelIndex index;
+    index.ptr = ptr;
+    index.type = type;
+    index.parent = parent;
+    indices.append(index);
+    return createIndex(row, column, indices.count() - 1);
+}
+
+QModelIndex qtSDLTreeModel::parent(const QModelIndex &child) const {
+    return indices[child.internalId()].parent;
 }
 
 QModelIndex qtSDLTreeModel::index(int row, int column, const QModelIndex& parent) const {
     if(parent.isValid()) {
-
+        SDLModelIndex myParent = indices[parent.internalId()];
+        switch(myParent.type) {
+        case kSDR:
+            return ICreateIndex(row, column, parent, myParent.sdr->get(row), kVar);
+        case kVar:
+            if(myParent.sv->getDescriptor()->getType() == plVarDescriptor::kStateDescriptor)
+                return ICreateIndex(row, column, parent, ((plSDStateVariable*)myParent.sv)->Record(row), kSDR);
+            else
+                return ICreateIndex(row, column, parent, myParent.sv, kVal);
+        case kVal:
+            return QModelIndex(); // this is not valid, values have no children
+        }
     }else{
-        return createIndex(row, column, sdl->get(row));
+        return ICreateIndex(row, column, parent, sdl, kSDR);
     }
 }
 
 int qtSDLTreeModel::rowCount(const QModelIndex& parent) const {
     if(parent.isValid()) {
-        plStateVariable* var = parent.internalPointer();
-        plVarDescriptor* desc = var->getDescriptor();
-        if(desc->getType() == plVarDescriptor::kStateDescriptor) {
-            return ((plSDStateVariable*)var)->getCount();
-        }else{
-            return ((plSimpleStateVariable*)var)->getCount();
+        SDLModelIndex myParent = indicies[parent.internalId()];
+        switch(myParent.type) {
+        case kSDR:
+            return myParent.sdr->getNumVars();
+        case kVar:
+            return myParent.sv->getCount();
+        case kVal:
+            return 0;
         }
     }else{
         return sdl->getNumVars();
@@ -34,8 +61,25 @@ int qtSDLTreeModel::columnCount(const QModelIndex& parent) const {
 
 QVariant qtSDLTreeModel::data(const QModelIndex& index, int role = Qt::DisplayRole) const {
     if(index.isValid()) {
-        plStateVariable* var = index.internalPointer();
-        plVarDescriptor* desc = var->getDescriptor();
-        return desc->getName();
+        SDLModelIndex myIndex = indices[index.internalId()];
+        switch(myIndex.type) {
+        case kSDR:
+            return QVariant(myIndex.sdr->getDescriptor()->getName().cstr());
+        case kVar:
+            return QVariant(myIndex.sv->getDescriptor()->getName().cstr());
+        case kVal:
+            {
+                plSimpleStateVariable* var = myIndex.sv;
+                switch(var->getDescriptor()->getType()) {
+                case plVarDescriptor::kBool:
+                    return QVariant(var->Bool(index.row()) ? "True" : "False");
+                case plVarDescriptor::kInt:
+                    return QVariant(plString::Format("%d", var->Int(index.row())).cstr());
+                default:
+                    return QVariant("Unhandled SDL Var Type");
+                }
+            }
+        }
     }
+    return QVariant("Requested invalid index");
 }
--- qtSDLTreeModel.h
+++ qtSDLTreeModel.h
@@ -1,18 +1,34 @@
 #ifndef QTSDLTREEMODEL_H
 #define QTSDLTREEMODEL_H
 
-#include "SDL/plStateDataRecord.h"
+#include <QVector>
 #include <QAbstractItemModel>
+
+class plStateDataRecord;
+class plStateVariable;
 
 class qtSDLTreeModel : public QAbstractItemModel
 {
     Q_OBJECT
 private:
     plStateDataRecord* sdl;
+    enum ItemType { kSDR, kVar, kVal };
+    struct SDLModelIndex {
+        QModelIndex parent;
+        ItemType type;
+        union {
+            plStateDataRecord* sdr;
+            plStateVariable* sv;
+        } ptr;
+    };
+    QVector<SDLModelIndex> indices;
+
+    QModelIndex ICreateIndex(int row, int column, QModelIndex parent, void* ptr, ItemType type);
 
 public:
     explicit qtSDLTreeModel(plStateDataRecord* sdl);
     QModelIndex index(int row, int column, const QModelIndex& parent) const;
+    QModelIndex parent(const QModelIndex &child) const;
     int rowCount(const QModelIndex& parent) const;
     int columnCount(const QModelIndex& parent) const;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
