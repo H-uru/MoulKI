e043e3dac7de0d6e6e758a19a388108bfd214baa
NadnerbD <brendandorr@gmail.com>
2012-02-19 17:59:16 -0500

    Add viewing support for more SDL var types

--- qtSDLTreeModel.cpp
+++ qtSDLTreeModel.cpp
@@ -1,6 +1,8 @@
 #include "qtSDLTreeModel.h"
 #include "SDL/plStateDataRecord.h"
 #include "SDL/plStateVariable.h"
+#include <Stream/hsRAMStream.h>
+#include <QStringList>
 
 qtSDLTreeModel::qtSDLTreeModel(plStateDataRecord* sdl) :
     QAbstractItemModel(0), sdl(sdl)
@@ -71,6 +73,28 @@ int qtSDLTreeModel::columnCount(const QModelIndex&) const {
     return 1;
 }
 
+// gets a prc string for any pointer
+// to an object with a prcWrite method
+template <class T>
+QString getPrc(T v) {
+    // make a prc object and write to it
+    hsRAMStream s(PlasmaVer::pvMoul);
+    pfPrcHelper prc(&s);
+    v->prcWrite(&prc);
+    // extract the contents of the stream
+    char* str = new char[s.size() + 1];
+    str[s.size()] = 0;
+    s.copyTo(str, s.size());
+    QString out(str);
+    delete[] str;
+    // strip off the xml header
+    QStringList lst = out.split('\n');
+    out = QString();
+    for(int i = 2; i < lst.count(); i++)
+        out.append(lst[i]);
+    return out;
+}
+
 QVariant qtSDLTreeModel::data(const QModelIndex& index, int role) const {
     if(role != Qt::DisplayRole)
         return QVariant();
@@ -89,8 +113,33 @@ QVariant qtSDLTreeModel::data(const QModelIndex& index, int role) const {
                     return QVariant(var->Bool(index.row()) ? "True" : "False");
                 case plVarDescriptor::kInt:
                     return QVariant(plString::Format("%d", var->Int(index.row())).cstr());
+                case plVarDescriptor::kByte:
+                    return QVariant(plString::Format("%d", var->Byte(index.row())).cstr());
+                case plVarDescriptor::kFloat:
+                    return QVariant(plString::Format("%f", var->Float(index.row())).cstr());
+                case plVarDescriptor::kDouble:
+                    return QVariant(plString::Format("%f", var->Double(index.row())).cstr());
+                case plVarDescriptor::kString:
+                    return QVariant(var->String(index.row()).cstr());
+                case plVarDescriptor::kChar:
+                    return QVariant(QString(var->Char(index.row())));
+                case plVarDescriptor::kKey:
+                    return QVariant(getPrc<plUoid*>(&var->Uoid(index.row())));
+                case plVarDescriptor::kCreatable:
+                    return QVariant(getPrc<plCreatable*>(var->Creatable(index.row())));
+                case plVarDescriptor::kRGB8:
+                case plVarDescriptor::kRGBA8:
+                    return QVariant(getPrc<hsColor32*>(&var->Color32(index.row())));
+                case plVarDescriptor::kRGB:
+                case plVarDescriptor::kRGBA:
+                    return QVariant(getPrc<hsColorRGBA*>(&var->ColorRGBA(index.row())));
+                case plVarDescriptor::kPoint3:
+                case plVarDescriptor::kVector3:
+                    return QVariant(getPrc<hsVector3*>(&var->Vector(index.row())));
+                case plVarDescriptor::kQuaternion:
+                    return QVariant(getPrc<hsQuat*>(&var->Quat(index.row())));
                 default:
-                    return QVariant("Unhandled SDL Var Type");
+                    return QVariant(plString::Format("Unhandled SDL Var Type (%d)", var->getDescriptor()->getType()).cstr());
                 }
             }
         }
